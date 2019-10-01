d1e186bed051f90ebe17d657180318e542a69e74
rick0404 <cortrale@gmail.com>
2019-10-01 12:48:29 -0500

    init

--- lp_thw.py
+++ lp_thw.py
@@ -202,17 +202,27 @@ def print_one(arg1):
 
 def print_none():
       print("I got nothin")
+
 print_two("anime", "soul")
 print_two_again("anime", "soul")
 print_one("First!")
 print_none()
 
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+      print(f"You have {cheese_count} cheeses!")
+      print(f"You have {boxes_of_crackers} boxes of crackers!")
+      print("Get a blanket. \n")
 
+      print("We can just give the function numbers directly:")
+      cheese_and_crackers(20,30)
+      print("Or, we can use variables from our script:")
+      amount_of_cheese = 10
+      amount_of_crackers = 50
+      cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+      print("And we can combine the two, variables and math:")
+      cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
 
-
-
-
-
+cheese_and_crackers(10,10)
 
 
 
